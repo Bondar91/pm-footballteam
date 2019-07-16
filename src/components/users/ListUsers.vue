a4e7a17692a62da9e645faf4bbfb797559a089b0
Krystian Bondaruk <krystian.bondaruk@gmail.com>
2019-07-16 23:31:07 +0200

    Creating a user display component with the API

--- src/components/users/ListUsers.vue
+++ src/components/users/ListUsers.vue
@@ -1,7 +1,52 @@
-<template></template>
+<template>
+  <div class="wrapper">
+    <table class="table">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Full Name</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>
+            <img :src="user.avatar" alt class="image" />
+          </td>
+          <td>{{`${user.first_name} ${user.last_name}`}}</td>
+          <td>
+            <a href="#" class="edit-link">
+              <i class="fas fa-edit"></i>
+            </a>
+            <a href="#" class="trash-link">
+              <i class="fas fa-trash"></i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  methods: {
+    getUsers() {
+      this.$http
+        .get("https://reqres.in/api/users")
+        .then(({ data }) => (this.users = data.data));
+      // .then({ data } => (this.users = data));
+    }
+  },
+  created() {
+    this.getUsers();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueAxios, axios);
+
 new Vue({
   router,
   render: h => h(App),
-}).$mount('.app');
+}).$mount('#app');
