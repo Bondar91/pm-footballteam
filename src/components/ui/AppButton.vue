8686194ce75beda0fbce5a7b15af9f468b8d66f3
Krystian Bondaruk <krystian.bondaruk@gmail.com>
2019-07-17 19:39:10 +0200

    create component AppButton

--- src/components/ui/AppButton.vue
+++ src/components/ui/AppButton.vue
@@ -15,6 +15,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+
+.btn {
+  font-size: 1.6rem;
+  text-decoration: none;
+  padding: 15px 30px;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  width: 140px;
+  height: 48px;
+
+  &--primary {
+    border-radius: 3rem;
+  }
+
+  &--icon {
+    background-image: url("../../assets/icons/plus.svg");
+    background-repeat: no-repeat;
+    background-size: 15px;
+    background-position: 20px 50%;
+    padding: 10px 20px 10px 40px;
+  }
+
+  &--green {
+    background-color: $green-btn;
+  }
+}
 </style>
-
-
